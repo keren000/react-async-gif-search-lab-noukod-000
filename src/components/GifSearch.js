957c971bc5ab53f644c4ac9f9e55cebf28692309
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-22 13:27:34 +0000

    Automatically backed up by Learn

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -9,16 +9,17 @@ class GifSearch extends Component {
   state = {
       query: ""
   }
+  
+  
+ handleSubmit = event => {
+   event.preventDefault();
+   this.props.fetchGIFs(this.state.query)
+ }
 
   handleChange = event => {
    this.setState(
      {query: event.target.value}
    );
- }
-
- handleSubmit = event => {
-   event.preventDefault();
-   this.props.fetchGIFs(this.state.query)
  }
 
  render() {
