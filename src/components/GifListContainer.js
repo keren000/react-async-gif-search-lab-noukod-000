5fc5efe1f1981cd06fda1755661a45c1dc1d36a2
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-22 13:40:02 +0000

    Done.

---
+++ src/components/GifListContainer.js
@@ -0,0 +1,36 @@
+//Write your code here!
+import React, { Component } from 'react'
+import GifList from '../components/GifList'
+import GifSearch from '../components/GifSearch'
+
+class GifListContainer extends React.Component {
+  state = {
+   		gifs: []
+  }
+
+  render() {
+    return (
+      <div>
+      < GifList gifs={this.state.gifs} />
+        <GifSearch searchSubmit={this.searchSubmit} />
+
+      </div>
+    )
+  }
+
+  searchSubmit = (query = "jellyfish") => {
+  	fetch('http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          gifs: data.data.slice(0,3)
+        })
+      })
+  }
+   componentDidMount() {
+    this.searchSubmit();
+  }
+
+}
+
+export default GifListContainer 
